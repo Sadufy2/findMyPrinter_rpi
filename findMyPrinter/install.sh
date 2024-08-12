a237e26101b37a0978a607141fd5088404f598d8
sadufy2 <sadufy2@gmail.com>
2024-08-12 15:57:33 +0200

    asd

--- findMyPrinter/install.sh
+++ findMyPrinter/install.sh
@@ -33,17 +33,12 @@ pip3 install Flask --break-system-packages
 
 echo "Installation complete!"
 
-# Define the line to add to .bashrc
-LINE="sudo python3 /home/findMyPrinter_rpi/findMyPrinter/comController.py"
-cd "/"
 
-# Remove any existing lines from .bashrc that match the pattern
-echo "Removing existing lines from .bashrc..."
-grep -vF "$LINE" ~/.bashrc > ~/.bashrc.tmp && mv ~/.bashrc.tmp ~/.bashrc
+cd "/"
 
 # Add the line to .bashrc
 echo "Adding line to .bashrc..."
-echo "$LINE" >> ~/.bashrc
+echo "sudo python3 /home/findMyPrinter_rpi/findMyPrinter/comController.py" >> ~/.bashrc
 
 echo "Line added to .bashrc"
 echo "Displaying the last 5 lines of .bashrc:"
