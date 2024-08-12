df292fc81cac4e82ae95f5e7553ed9ae24c68718
sadufy2 <sadufy2@gmail.com>
2024-08-12 14:22:57 +0200

    install added

---
+++ findMyPrinter/install.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+# Exit the script if any command fails
+set -e
+
+echo "Updating and upgrading the system..."
+sudo apt-get update
+sudo apt-get upgrade -y
+
+echo "Installing Git..."
+sudo apt-get install git -y
+
+echo "Cloning the GitHub repository..."
+REPO_URL="https://github.com/Sadufy2/findMyPrinter_rpi.git"
+CLONE_DIR="repository"
+
+if [ -d "$CLONE_DIR" ]; then
+  echo "Repository already cloned. Pulling the latest changes..."
+  cd "$CLONE_DIR"
+  git pull
+else
+  git clone "$REPO_URL"
+  cd "$CLONE_DIR"
+fi
+
+LINE="sudo python3 /home/findMyPrinter/comController.py"
+if grep -Fxq "$LINE" ~/.bashrc
+then
+    echo "Line already exists in .bashrc"
+else
+    echo "Adding line to .bashrc..."
+    echo "$LINE" >> ~/.bashrc
+    echo "Line added to .bashrc"
+fi
+
+echo "Installing Python and Pip..."
+sudo apt-get install python3 python3-pip -y
+
+echo "Installing Flask..."
+pip3 install Flask
+
+echo "Installation complete!"
